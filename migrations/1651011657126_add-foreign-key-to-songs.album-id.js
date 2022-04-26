81ab0b2c775a05c15d402a28dfe9592f17eefc5e
Rocky Prabowo <rocky@lazycats.id>
2022-04-27 05:21:26 +0700

    feat: normalise songs and albums table - add a default album - update songs without album_id to the default album - add foreign key constraints on songs.album_id that refers to albums.id

---
+++ migrations/1651011657126_add-foreign-key-to-songs.album-id.js
@@ -0,0 +1,44 @@
+/* eslint-disable camelcase */
+
+/**
+ * @typedef {import('node-pg-migrate').MigrationBuilder} MigrationBuilder
+ */
+
+exports.shorthands = undefined
+
+/**
+ * Migration: up
+ *
+ * @param {MigrationBuilder} pgm Migration Builder
+ */
+exports.up = pgm => {
+  // Add a default album
+  pgm.sql("INSERT INTO albums(id, name, year) VALUES ('album-default', '<No Album>', 0)")
+
+  // Set every null album_id to the default album
+  pgm.sql("UPDATE songs SET album_id = 'album-default' WHERE album_id = NULL")
+
+  // Set default value to 'album-default'
+  pgm.alterColumn('songs', 'album_id', {
+    default: 'album-default'
+  })
+
+  // Add foreign key constraint on `album_id` column in `songs` that refers to `id` column in `albums`
+  pgm.addConstraint('songs',
+    'fk_songs.album_id_albums.id',
+    'FOREIGN KEY(album_id) REFERENCES albums(id) ON DELETE SET DEFAULT'
+  )
+}
+
+/**
+ * Migration: down
+ *
+ * @param {MigrationBuilder} pgm Migration Builder
+ */
+exports.down = pgm => {
+  pgm.dropConstraint('songs', 'fk_songs.album_id_albums.id')
+
+  pgm.sql("UPDATE songs SET album_id = NULL WHERE album_id = 'album-default'")
+
+  pgm.sql("DELETE FROM albums WHERE id = 'album-default'")
+}
