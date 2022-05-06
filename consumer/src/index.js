451c552ff390714f7adee73bf55bf0dde754e1d9
Rocky Prabowo <rocky@lazycats.id>
2022-05-07 00:05:54 +0700

    fix: load dotenv on consumer application

--- consumer/src/index.js
+++ consumer/src/index.js
@@ -1,3 +1,5 @@
+require('dotenv').config({ path: require('path').join(__dirname, '../../.env') })
+
 const amqp = require('amqplib')
 const PlaylistService = require('@openmusic/common/services/postgresql/playlists_service')
 const PlaylistExportMailer = require('./mailers/playlist_export_mailer')
